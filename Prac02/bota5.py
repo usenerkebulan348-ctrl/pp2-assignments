018bd24f58c625e75ca194a5615f983239dde967
ErkebulanUsen <usenerkebulan348@gmail.com>
2026-02-02 00:22:16 +0600

    int startes at 0

--- Prac02/bota5.py
+++ Prac02/bota5.py
@@ -1,6 +1,7 @@
 a = int(input())
+n = list(map(int,input().split()))
+maxx = max(n)
 
-if a > 0 and (a & (a - 1)) == 0:
-    print("YES")
-else:
-    print("NO") 205
+for i in range(a):
+    if n[i] == maxx:
+        print(i+1) 
