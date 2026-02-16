1f6238ba755d9e387d07a5199d502de555d3f540
ErkebulanUsen <usenerkebulan348@gmail.com>
2026-02-16 13:27:31 +0600

    74

---
+++ Prac03/e8.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return False
+        self.balance -= amount
+        return True
+
+
+B, W = map(int, input().split())
